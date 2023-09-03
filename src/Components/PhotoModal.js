39dc42a676cb10e52a1d25a41915b2831ebedf1a
kinwjdghks <kinwjdghks@naver.com>
2023-09-04 01:29:36 +0900

    making design4

--- src/Components/PhotoModal.js
+++ src/Components/PhotoModal.js
@@ -31,6 +31,9 @@ import design1_vertical from "../assets/skins/design1_vertical.svg";
 import design2_square from "../assets/skins/design2_square.svg";
 import design2_horizontal from "../assets/skins/design2_horizontal.svg";
 import design2_vertical from "../assets/skins/design2_vertical.svg";
+import design3_square from "../assets/skins/design3_square.svg";
+import design3_horizontal from "../assets/skins/design3_horizontal.svg";
+import design3_vertical from "../assets/skins/design3_vertical.svg";
 
 const BackDrop = () => {
   return <div className={styles.backdrop}></div>;
@@ -250,26 +253,40 @@ const Modal = ({ onToggleModalHandler, modalOpened }) => {
       </>
     );
   } else if (skinIdx === 3) {
-    // const image =
-    //   vidConfigIdx === 0
-    //     ? design1_square
-    //     : vidConfigIdx === 1
-    //     ? design1_vertical
-    //     : design1_horizontal;
-    // skinElement = (
-    //   <>
-    //     {vidConfigIdx === 1 && <div></div>}
-    //     <img className={styles.skinElement} src={image} width="933" />
-    //   </>
-    // );
+    const image =
+      vidConfigIdx === 0
+        ? design3_square
+        : vidConfigIdx === 1
+        ? design3_vertical
+        : design3_horizontal;
+      const width_ =
+      vidConfigIdx === 0
+        ? 960
+        : vidConfigIdx === 1
+        ? 1150
+        : 880;
+    skinElement = (
+      <>
+        {vidConfigIdx === 1 && <div>
+          <div className={styles.skkucomit}>
+            <p className={styles.skku}>SKKU</p>
+            <p className={styles.comit}>COMIT</p>
+          </div></div>}
+        <img className={`${styles.skinElement} ${styles.design3} ${classNameByConfig}`} src={image} width={width_} />
+      </>
+    );
   } else if (skinIdx === 4) {
-    // if (vidConfigIdx === 0) {
-    //   setSkinElement();
-    // } else if (vidConfigIdx === 1) {
-    //   setSkinElement();
-    // } else {
-    //   setSkinElement();
-    // }
+    skinElement = (
+      <>
+        {vidConfigIdx === 0 && 
+          <div className={styles.borderText}>
+            <p className={styles.up}>COMIT FILM</p>
+            <p className={styles.right}></p>
+            <p className={styles.left}>COMIT</p>
+            <p className={styles.down}>COMIT</p>
+          </div>}
+      </>
+    );
   }
   return (
     <div className={styles.container}>
