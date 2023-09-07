ecd1170ade2cf312cc6542676876b846c6d29de2
kinwjdghks <kinwjdghks@naver.com>
2023-09-07 17:50:00 +0900

    merge

---
+++ src/Components/Note.js
@@ -0,0 +1,14 @@
+
+import styles from './Note.module.css';
+
+const Note = ({data}) => {
+    
+
+    return <div className={styles.container}>
+        <div className={styles.textField}>
+            
+        </div>
+    </div>;
+}
+
+export default Note;
